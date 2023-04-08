b503335893af122d27a086d4ce5527bf99d24b58
koha90 <koha90@github.com>
2023-04-08 16:10:08 +0500

    function with dynamic route

--- hello.py
+++ hello.py
@@ -12,4 +12,12 @@ def index():
     return "<h1>Hello world, from flask</h1>"
 
 
+@app.route("/user/<name>")
+def user(name):
+    """
+    user: function for rendering html with
+    """
+    return f"<h2>Hello, {name}!"
+
+
 app.add_url_rule("/", "index", index)
